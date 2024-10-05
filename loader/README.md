982edb3fdd99757f6dc2bbf861950f1f59f5a044
DoctorSlimm <87606888+DoctorSlimm@users.noreply.github.com>
2024-10-05 15:27:04 +0100

    gitfuck

---
+++ loader/README.md
@@ -0,0 +1,6 @@
+##### Different loaders for context competitor
+
+
+- github issues
+- github organizations and accounts
+- other.
---
+++ loader/github_loader.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+
+### Load Recent Issues from relevant repositories for keywords.
+#
+# - search or find github repositories by keywords.
+# - load and filter github repositories by #issues and #stars.
+# - extract issues across all filtered repositories.

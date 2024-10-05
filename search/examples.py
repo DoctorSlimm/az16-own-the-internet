1c5b65f262a8b93760ad5dd7e862fa672aa959af
Red-Cardigan <jethro.reeve@gmail.com>
2024-10-05 18:01:22 +0100

    Update agent

---
+++ search/examples.py
@@ -0,0 +1,26 @@
+
+
+examples = [
+    {
+        
+        "description": "knowledge base for financial documents",
+
+        "requirements": ["LLM", "vector database", "keyword search"],
+
+        "providers": ["OpenAI", "Pinecone", "Elasticsearch"],
+
+        "packages": ["openai", "pinecone", "elasticsearch"],
+    },
+    {
+        "description": "game development on solana blockchain",
+
+        "search_queries": ["how to build games on solana?"],
+
+        "urls": [
+            "https://solana.com/developers/guides/games/getting-started-with-game-development",
+            "https://www.reddit.com/r/solana/comments/19e9l78/i_want_to_develop_games_on_solana_are_you/"
+        ],
+
+        "requirements": [""],
+    }
+]
